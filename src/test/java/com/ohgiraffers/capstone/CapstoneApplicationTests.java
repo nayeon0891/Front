3363b423429f0c880c48bd23949718480a03374a
nayeon <nayeon08911@gmail.com>
2024-11-01 19:58:47 +0900

    00

---
+++ src/test/java/com/ohgiraffers/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
